ef3f73bd7a47f62cdac67678a00ae3d9ccfeda28
Kumar Gautam <22.gautam@gmail.com>
2015-04-17 19:17:59 +0530

    adding the modified cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	inv <- NULL
+	  set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	  }
+	  get <- function() x
+	  set.inv.matrix <- function(inv.matrix) inv <<- inv.matrix
+	  get.inv.matrix <- function() inv
+	  list(set = set, get = get,
+		   set.inv.matrix = set.inv.matrix,
+		   get.inv.matrix = get.inv.matrix)
+
 }
 
 
@@ -12,4 +24,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+	inv.matrix <- x$get.inv.matrix()
+	  
+	  if(!(is.null(inv.matrix))){
+		message("getting the cached data")
+		return (inv.matrix)
+	  }
+	  
+	  data.matrix <- x$get()
+	  inv.matrix <- solve(data.matrix)
+	  
+	  x$set.inv.matrix(inv.matrix)
+	  inv.matrix
 }
